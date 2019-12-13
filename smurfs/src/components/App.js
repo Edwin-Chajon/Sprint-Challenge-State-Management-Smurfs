8f668b0077784bfa16cbf213f0c72b982db8cd7b
Edwin Chajon <junchajo@gmail.com>
2019-12-13 12:00:03 -0800

    rework

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,13 +1,42 @@
 import React, { Component } from "react";
 import "./App.css";
+import axios from 'axios'
+
+import smurfContext from '../smurfContext/smurfContext'
+
+
+
 class App extends Component {
+
+  state = {
+    smurfs: [],
+    smurfsText: ''
+  };
+
+  componentDidMount(){
+    axios.get('http://localhost:3333/smurfs').then(response => {
+      this.setState({smurfs: response.data})
+    })
+    window.addEventListener('resize', this.handleResize);
+  }
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleResize);
+  }
+
   render() {
     return (
       <div className="App">
         <h1>SMURFS! 2.0 W/ Redux</h1>
         <div>Welcome to your state management version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
+        <div className="players">
+        {this.state.smurfs.map(smurf=>(
+          <div>
+          <h2>{smurf.name}</h2>
+          <h3>{smurf.age}</h3>
+          <h3>{smurf.height}</h3>
+          </div>
+        ))}
+      </div>
       </div>
     );
   }
